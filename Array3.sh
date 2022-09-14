d1a761a3b0f64674e9acc9365fab40ccdf76b242
Devika18 <52191935+Devika18@users.noreply.github.com>
2022-09-14 09:54:50 +0530

    Update Array3.sh

--- Array3.sh
+++ Array3.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-for ((i=1; i<=10; i++))
-do
-   r=$(( $i % 2))
-	if [ $r -ne 0 ]
-	then
-          #echo "Prime Numbers are: $i"
-	  count=1
-	  for((j=1;j<i+1;j++))
-	    do
-		count=$((count*j))
-	  done
-		echo "Factorial of $i is: "$count
-		array=a[$count]
-		echo $array
-        fi
+read -p "Enter a number: " n
+   for ((i=2; i<=$n; i++))
+   do
+
+     if (($n%i==0))
+     then
+        isPrime=1
+        for ((j=2; j<=i/2; j++))
+        do
+         if (($i%$j==0))
+         then
+          isPrime=0
+         break
+         fi
+        done
+
+if (($isPrime==1))
+then
+  factor[c]=$i
+  c=$c+1
+fi
+    fi
 done
+
+echo "Prime Factors are: "
+echo ${factor[@]}
+
